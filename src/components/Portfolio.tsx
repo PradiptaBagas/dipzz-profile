5ed6bf79104672a4c77f0d4678cd1360479f766b
PradiptaBagas <Pradiptabagas10@gmail.com>
2026-03-20 17:21:13 +0700

    update project img

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -253,7 +253,7 @@ const ProjectsSection = () => {
       title: "Shadeart", 
       desc: "Gallery Poster by Shade.", 
       tags: ["React", "Tailwind", "TypeScript", "Vite"],
-      image: "/art.jpg",
+      image: "/artshade.png",
       link: "https://shadeart.vercel.app/"
     },
     { 
